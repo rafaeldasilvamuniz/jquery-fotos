a24991b49fe7a5c5aea2f9a4faa509982e7bff1d
Rafael da Silva Muniz <rafaelmunizrsm@hotmail.com>
2023-05-20 23:10:29 -0300

    jquery

---
+++ main.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        //alert("Expandir formulário")
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e){
+        console.log("submit");
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        //console.log(enderecoDaNovaImagem);
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+           <div class="overlay-imagem-link">
+             <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+             </a>
+           </div>
+           `).appendTo(novoItem); 
+           $(novoItem).appendTo('ul');
+           $(novoItem).fadeIn(5000);//5000 milisegundos igual a 5seg
+           $('#endereco-imagem-nova').val('')
+    }) 
+   // console.log(document.querySelector('header button'));
+   // console.log($('#botao-cancelar'));
+})
+
+/*const form = document.getElementById('form-atividade');
+const nom = [];
+const tel = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    adicionaLinha();
+    atualizaTabela();
+
+
+});    
+
+function adicionaLinha() {    
+    const inputNome = document.getElementById('nome');
+    const inputTel = document.getElementById('telefone');
+   // var SPMaskBehavior inputTel.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+       
+    nom.push(inputNome.value);
+    tel.push(inputTel.value);
+   // let linhas = '';
+
+    let linha = '<tr>';
+    linha += `<th>${inputNome.value}</th>`;
+    linha += `<th>${(inputTel.value)}</th>`;
+    //linha += `<td>${(inputTel.value).length === 11 ? '(00)00000-0000' : '(00)00000-0000'}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNome.value = '';
+    inputTel.value = '';
+    
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+}
+*/
+
+
+/*<script type="text/javascript">
+		$(document).ready(function(){
+
+		var SPMaskBehavior = function (val) {
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		},
+		spOptions = {
+			onKeyPress: function(val, e, field, options) {
+			field.mask(SPMaskBehavior.apply({}, arguments), options);
+			}
+		};
+		$("#telefone").mask(SPMaskBehavior, spOptions);	
+
+		})
+	</script>*/
